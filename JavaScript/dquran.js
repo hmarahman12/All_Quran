b4d5f5d30cf72582efd89a19059505b8a3a374f8
MdRahman1234 <84412941+hmarahman12@users.noreply.github.com>
2025-05-10 19:52:23 +0600

    Update dquran.js

---
+++ JavaScript/dquran.js
@@ -0,0 +1,5 @@
+
+if(window.innerWidth > 900){
+    document.querySelector(".page-wrapper:nth-child(4) div:first-child").classList.add("newClass");
+    document.querySelector(".page-wrapper:nth-child(5) div:first-child").classList.add("newClass");
+}
